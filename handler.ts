03c81cac511c9e2cfc00beec11bc14ef91d1964a
Wendel Costa <wendelcosta@Wendel-Costas-MacBook-Pro-13.local>
2020-09-04 14:08:05 +1000

    Added aplication type

--- handler.ts
+++ handler.ts
@@ -17,6 +17,7 @@ export const comics: Handler = async () => {
   return {
     statusCode: 200,
     headers: {
+      'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
